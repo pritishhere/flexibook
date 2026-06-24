34e209fe783da6b025b4a58d9c734ca6532ae4ea
pritishhere <ghoshpritish111@gmail.com>
2026-06-24 17:04:00 +0530

    Initial setup

---
+++ FlexiBook/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='bg-gray-400 text-black-800 min-h-screen flex flex-col items-center justify-center'>
+      <h1>Welcome to FlexiBook</h1>
+      <p>This is a sample React application.</p>
+    </div>
+  );
+}
+
+export default App;

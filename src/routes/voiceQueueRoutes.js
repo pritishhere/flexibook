33aa6880cb7978fe7ad20ad0bb92eca82cff7659
pritam26-arch <pritampandey307@gmail.com>
2026-07-05 06:24:29 +0530

    check

--- src/routes/voiceQueueRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handleIncomingWhatsAppMessage } = require('../controllers/voiceQueueController');
-
-// 🌐 POST /api/voice-queue/whatsapp - Twilio Webhook Receiver for incoming messages/voice notes
-router.post('/whatsapp', handleIncomingWhatsAppMessage);
-
-module.exports = router;

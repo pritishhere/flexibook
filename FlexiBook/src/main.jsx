774751c6a32a346ce94f185a66dad3800f77c3fb
pritam26-arch <pritampandey307@gmail.com>
2026-07-07 15:31:53 +0530

    Final Home

--- FlexiBook/src/main.jsx
+++ FlexiBook/src/main.jsx
@@ -1,14 +1,11 @@
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// Naye context folder se ThemeProvider import kar liya
-import { ThemeProvider } from './context/ThemeContext.jsx' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
 )
